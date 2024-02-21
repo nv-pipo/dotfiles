23bd2bcf99a407dd169d02a27b65e8600fd15e6e
Nicolas Velasquez <gnicolax@gmail.com>
2024-02-21 13:04:11 +0100

    Use stow for linking dotfiles

--- install_macos.sh
+++ install_macos.sh
@@ -8,14 +8,13 @@ dotfiles_path=`pwd`
 
 # configure
 # oh-my-zsh
-ln -s $dotfiles_path/zsh/oh-my-zsh ~/.oh-my-zsh
-ln -s $dotfiles_path/zsh/.zshrc ~/.zshrc
 git clone https://github.com/esc/conda-zsh-completion ~/.oh-my-zsh/custom/plugins/conda-zsh-completion
 
 # powerlevel10k
 ln -s $dotfiles_path/zsh/powerlevel10k ~/.oh-my-zsh/custom/themes
-ln -s $dotfiles_path/zsh/.p10k.zsh ~/.p10k.zsh
 
 # tmux
-ln -s $dotfiles_path/tmux/.tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# link files to parent
+stow . -v
