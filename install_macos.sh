c928e8c2e892475d62bc972c456e86fc05f6a791
Nicolas Velasquez <gnicolax@gmail.com>
2024-02-21 12:18:27 +0100

    Remove exa install from install_macos.sh

--- install_macos.sh
+++ install_macos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # dependencies
-brew install neofetch onefetch htop wget nvim ripgrep tmux lua exa zoxide
+brew install neofetch onefetch htop wget nvim ripgrep tmux lua zoxide
 
 # vars
 dotfiles_path=`pwd`
