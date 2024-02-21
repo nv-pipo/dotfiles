6a7349d944ef6e4a2d31481bc0a7039318bd20c8
Nicolas Velasquez <gnicolax@gmail.com>
2024-02-21 13:04:59 +0100

    Clone repos omz and plugins instead of using submodules

--- install_macos.sh
+++ install_macos.sh
@@ -8,10 +8,11 @@ dotfiles_path=`pwd`
 
 # configure
 # oh-my-zsh
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone https://github.com/esc/conda-zsh-completion ~/.oh-my-zsh/custom/plugins/conda-zsh-completion
 
 # powerlevel10k
-ln -s $dotfiles_path/zsh/powerlevel10k ~/.oh-my-zsh/custom/themes
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
 # tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
