2fe33f89759295be43725d513905ab9a00f03de2
Nicolas Velasquez <gnicolax@gmail.com>
2024-05-20 16:04:35 +0200

    chore: Create Linux installation script

---
+++ install_linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# dependencies
+sudo apt install htop wget ripgrep tmux zoxide fzf stow micro
+
+# vars
+dotfiles_path=`pwd`
+
+# dependencies
+## zinit
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+mkdir -p "$(dirname $ZINIT_HOME)"
+git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+
+## tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# link files to parent
+stow . -v
