8320ddcbd7f0c480f5416bcff32fbb4ff124caef
Nicolas Velasquez <gnicolax@gmail.com>
2024-05-21 21:41:05 +0200

    chore: Update install_macos.sh to include 'tlrc' in the list of installed dependencies

--- install_macos.sh
+++ install_macos.sh
@@ -2,7 +2,7 @@
 
 # dependencies
 brew tap homebrew/cask-fonts
-brew install neofetch onefetch htop wget ripgrep tmux zoxide fzf stow font-meslo-for-powerlevel10k micro git-delta bat ripgrep
+brew install neofetch onefetch htop wget ripgrep tmux zoxide fzf stow font-meslo-for-powerlevel10k micro git-delta bat ripgrep tlrc
 
 # vars
 dotfiles_path=`pwd`
