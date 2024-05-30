9ba2f39c0151ad29019e085fd8b7add1e85800ab
Nicolas Velasquez <gnicolax@gmail.com>
2024-05-30 14:09:12 +0200

    chore: Update install_linux.sh and install_macos.sh to include 'eza' in the list of installed dependencies

--- install_linux.sh
+++ install_linux.sh
@@ -11,7 +11,7 @@ set -e
 doas apk update
 doas apk add --upgrade apk-tools
 doas apk upgrade --available
-doas apk add htop wget ripgrep tmux zoxide stow micro zsh delta bat ripgrep btop fzf dust
+doas apk add htop wget ripgrep tmux zoxide stow micro zsh delta bat ripgrep btop fzf dust eza
 
 # vars
 dotfiles_path=`pwd`
