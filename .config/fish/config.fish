ef983e7aad79ca0adfe2548779af22a3568aaec6
Nicolas Velasquez <gnicolax@gmail.com>
2024-06-10 16:20:10 +0200

    chore: Update fish config to not initialize conda, instead define alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,16 +28,6 @@ if test -e ~/homebrew
     alias cnf="brew which-formula --explain "
 end
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f $HOME/miniforge3/bin/conda
-    eval $HOME/miniforge3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "$HOME/miniforge3/etc/fish/conf.d/conda.fish"
-        . "$HOME/miniforge3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "$HOME/miniforge3/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
-conda activate py3_12-shell
+alias init_conda="eval $HOME/miniforge3/bin/conda \"shell.fish\" \"hook\" $argv | source"
+
+
