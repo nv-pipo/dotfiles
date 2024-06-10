90e2c207c1cee59878a90058df6bbe26ed6b853b
Nicolas Velasquez <gnicolax@gmail.com>
2024-06-10 14:24:11 +0200

    chore: Installation scripts, make sure only alacritty conf file is stowed

--- install_macos.sh
+++ install_macos.sh
@@ -13,6 +13,9 @@ ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 mkdir -p "$(dirname $ZINIT_HOME)"
 git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 
+## alacritty
+mkdir -p ~/.config/tmux
+
 ## tmux
 mkdir -p ~/.config/tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
