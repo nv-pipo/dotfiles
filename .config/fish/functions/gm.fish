3d970b4eea2beccd3dc13961f7451b3a0447b143
Nicolas Velasquez <gnicolax@gmail.com>
2024-06-13 15:24:18 +0200

    feat: Add gm function for performing git merge from source to destination branch

---
+++ .config/fish/functions/gm.fish
@@ -0,0 +1,16 @@
+function gm  --description "Do git merge from branches source -> dest"
+    if test (count $argv) -ne 2
+        echo "Usage: gm source dest"
+        return 1
+    end
+    echo "Doing git merge from $argv[1] -> $argv[2]"
+    # Save current branch
+    set current_branch (git branch --show-current)
+    # Do the merge
+    git checkout $argv[2]
+    git merge $argv[1]
+    # Push the changes
+    git push
+    # Go back to the original branch
+    git checkout $current_branch
+end
