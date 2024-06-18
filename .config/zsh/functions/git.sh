73f5977189a56dfbc321b2737964b6938fe58913
Nicolas Velasquez <gnicolax@gmail.com>
2024-06-18 10:51:20 +0200

    feat: Add git merge function to simplify merging branches

---
+++ .config/zsh/functions/git.sh
@@ -0,0 +1,18 @@
+gm() {
+    if [[ $# -ne 2 ]]; then
+        echo "Usage: gm source dest"
+        return 1
+    fi
+    echo "Doing git merge from $1 -> $2"
+    # Save current branch
+    current_branch=$(git branch --show-current)
+    # stash everything
+    git stash --include-untracked
+    # Do the merge
+    git checkout $2
+    git merge $1
+    # Push the changes
+    git push
+    # Go back to the original branch
+    git checkout $current_branch
+}
