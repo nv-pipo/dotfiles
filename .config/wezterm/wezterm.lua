ba81beea66669f3edec297929dbaf2c26e739fba
Nicolas Velasquez <gnicolax@gmail.com>
2024-08-28 16:03:19 +0200

    chore: Reformat wezterm config file

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -8,35 +8,35 @@ config.enable_kitty_keyboard = true
 config.enable_csi_u_key_encoding = false
 
 -- For example, changing the color scheme:
-config.color_scheme = "Tokyo Night"
+config.color_scheme = 'Tokyo Night'
 
 -- Disable tabs
 config.enable_tab_bar = false
 
 -- Remove the window decorations
-config.window_decorations = "RESIZE"
+config.window_decorations = 'RESIZE'
 
 -- Start terminal maximized
 local mux = wezterm.mux
-wezterm.on("gui-startup", function()
-	local _, _, window = mux.spawn_window({})
-	window:gui_window():maximize()
+wezterm.on('gui-startup', function()
+    local _, _, window = mux.spawn_window {}
+    window:gui_window():maximize()
 end)
 
 -- Set keybindings
 config.keys = {
-   {
-       key = "?",
-       mods = "SHIFT | CMD",
-       action = wezterm.action.ActivateCommandPalette,
-   },
-   -- Make Option-Backspace equivalent to Alt-Backspace which many line editors interpret as backward-kill-word
-   { key = "Backspace", mods = "OPT", action = wezterm.action({ SendString = "\x17" }) },
-   { key = "Delete", mods = "OPT", action = wezterm.action({ SendKey = { key = "Delete", mods = "ALT" } }) },
-   -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-   { key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendKey = { key = "b", mods = "ALT" } }) },
-   -- Make Option-Right equivalent to Alt-f; forward-word
-   { key = "RightArrow", mods = "OPT", action = wezterm.action({ SendKey = { key = "f", mods = "ALT" } }) },
+    {
+        key = '?',
+        mods = 'SHIFT | CMD',
+        action = wezterm.action.ActivateCommandPalette,
+    },
+    -- Make Option-Backspace equivalent to Alt-Backspace which many line editors interpret as backward-kill-word
+    { key = 'Backspace', mods = 'OPT', action = wezterm.action { SendString = '\x17' } },
+    { key = 'Delete', mods = 'OPT', action = wezterm.action { SendKey = { key = 'Delete', mods = 'ALT' } } },
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    { key = 'LeftArrow', mods = 'OPT', action = wezterm.action { SendKey = { key = 'b', mods = 'ALT' } } },
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    { key = 'RightArrow', mods = 'OPT', action = wezterm.action { SendKey = { key = 'f', mods = 'ALT' } } },
 }
 
 config.debug_key_events = true
