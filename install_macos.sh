e702f15e4d181366f27c7d42e101cd89e43fd4d7
Nicolas Velasquez <gnicolax@gmail.com>
2024-09-02 12:49:51 +0200

    feat: Add Alacritty theme repository to macos install script

--- install_macos.sh
+++ install_macos.sh
@@ -55,5 +55,9 @@ conda config --set changeps1 False
 ## tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# We use Alacritty's default Linux config directory as our storage location here.
+mkdir -p ~/.config/alacritty/themes
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+
 # link files to parent
 stow . -v
