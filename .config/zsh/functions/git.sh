a9703c4dfbe40ee2def4f81ef6583bc969807791
Nicolas Velasquez <gnicolax@gmail.com>
2024-11-21 15:07:30 +0100

    feat(zsh): Enhance git merge function with no-ff option and debugging output

--- .config/zsh/functions/git.sh
+++ .config/zsh/functions/git.sh
@@ -6,15 +6,17 @@ gm() {
     echo "Doing git merge from $1 -> $2"
     # Save current branch
     current_branch=$(git branch --show-current)
+    set -x
     # stash everything
     git stash --include-untracked
     # Do the merge
     git checkout $2
-    git merge $1
+    git merge --no-ff $1
     # Push the changes
     git push
     # Go back to the original branch
     git checkout $current_branch
     # pop the stash
     git stash pop
+    set +x
 }
