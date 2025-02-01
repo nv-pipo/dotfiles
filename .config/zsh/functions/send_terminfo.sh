80506487298dd8876ed49ceb5da33662ad18af63
Nicolas Velasquez <gnicolax@gmail.com>
2025-02-01 16:42:08 +0100

    feat(terminfo): rename send_terminfo function and add send_terminfo_sudo for elevated permissions

--- .config/zsh/functions/send_terminfo.sh
+++ .config/zsh/functions/send_terminfo.sh
@@ -1,9 +1,15 @@
-send_terminfo() {
+send_terminfo_ssh() {
     if [[ $# -ne 1 ]]; then
         echo "Usage: send_terminfo SERVER"
         return 1
     fi
     set -x
     infocmp -x | ssh $1 -- tic -x -
+    set +x
+}
+
+send_terminfo_sudo() {
+    set -x
+    infocmp -x | sudo tic -x -
     set +x
 }
